b78868af7a391be0ca44658c353b63e3d0a49114
Yash Kothari <yashkothari1000@gmail.com>
2019-12-13 20:37:46 +0530

    Initial Commit

---
+++ EconomicsProjectTests/EconomicsProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  EconomicsProjectTests.swift
+//  EconomicsProjectTests
+//
+//  Created by Valley on 13/12/19.
+//  Copyright © 2019 Valley. All rights reserved.
+//
+
+import XCTest
+@testable import EconomicsProject
+
+class EconomicsProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
