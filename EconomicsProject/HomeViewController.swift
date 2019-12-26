726df03a77d1ebc33e08d1c654b27d116bdf343f
Yash Kothari <yashkothari1000@gmail.com>
2019-12-26 14:40:29 +0530

    Level 3 Implementation complete

--- EconomicsProject/HomeViewController.swift
+++ EconomicsProject/HomeViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    // Content Handler Variables
+    var contentNum = 0
+    var contentLevel = 0
+    let content : [Int] = [3, 5, 5]
     
+    // Instance Outlets
     @IBOutlet weak var ChangeNameTextFieldView: UITextField!
     @IBOutlet weak var musicSwitch: UISwitch!
     @IBOutlet weak var soundSwitch: UISwitch!
@@ -43,10 +48,13 @@ class HomeViewController: UIViewController {
         
         // DialougeView
         DialogueLabelView.textColor = UIColor.white
+        
+        // DialogueRenderer
+        DialogueLabelView.text = content(num: 0, level: 0)
     }
     
     // Content Function
-    func content(num : Int, level : Int) {
+    func content(num : Int, level : Int) -> String {
         
         switch level {
             
@@ -54,50 +62,86 @@ class HomeViewController: UIViewController {
             switch num {
                 
             // Level #1
-            case 0: "Person X wants to set up their business of producing and selling furniture"
-            case 1: "Primary Sector: this sector functions by obtaining materials from nature and converting it into raw materials."
-            case 2: "Secondary Sector: this sector uses the raw materials to manufacture goods or services."
-            case 3: "Tertiary sector: this is the retail sector, responsible for selling the good or service."
+            case 0: return "Person X wants to set up their business of producing and selling furniture"
+            case 1: return "Primary Sector: this sector functions by obtaining materials from nature and converting it into raw materials."
+            case 2: return "Secondary Sector: this sector uses the raw materials to manufacture goods or services."
+            case 3: return "Tertiary sector: this is the retail sector, responsible for selling the good or service."
                 
             // Multiple Choice #1
                 
-            default: "Error Loading Content"
+            default: return "Error Loading Content"
             }
         
         case 1:
             switch num {
                 
             // Level #2
-            case 0: "In order to begin trading, person X needs to begin production. What factors of production will they use?"
-            case 1: "Factors of Production: There are 4 factors which are used together to produce a given good or a service. They are Land, Labour, Capital, and Enterprise."
-            case 2: "Land is all natural resources, eg. wood"
-            case 3: "Labour is human resources, eg. workers"
-            case 3: "Capital is manmade resources, eg. machinery"
-            case 4: "Enterprise is the risk-taking ability and the ability to combine the other three factors of production to produce goods or services"
+            case 0: return "In order to begin trading, person X needs to begin production. What factors of production will they use?"
+            case 1: return "Factors of Production: There are 4 factors which are used together to produce a given good or a service. They are Land, Labour, Capital, and Enterprise."
+            case 2: return "Land is all natural resources, eg. wood"
+            case 3: return "Labour is human resources, eg. workers"
+            case 4: return "Capital is manmade resources, eg. machinery"
+            case 5: return "Enterprise is the risk-taking ability and the ability to combine the other three factors of production to produce goods or services"
                 
             // Multiple Choice #2
                 
-            default: "Error Loading Content"
+            default: return "Error Loading Content"
             }
         
         case 2:
             switch num {
                 
             // Level #3
-            case 0: "Profit = selling price - cost price"
-            case 1: "There is an ‘economic problem’ which is known as scarcity. Wants are unlimited but the resources are limited."
-            case 2: "For example, Person x has $20. They can either buy a book worth $20 or shoes worth $20, but they cannot buy both as they only have $20."
-            case 3: "Therefore, they must make a choice, and the option they don’t choose becomes the Opportunity Cost."
-            case 4: "If Person X chooses the books over the shoes, shoes will be the opportunity cost because it is the next best alternative."
-            case 5: "Since you have only limited money due to scarcity, you can either produce chairs or tables. Which option is a better one for your furniture business?"
+            case 0: return "Profit = selling price - cost price"
+            case 1: return "There is an ‘economic problem’ which is known as scarcity. Wants are unlimited but the resources are limited."
+            case 2: return "For example, Person x has $20. They can either buy a book worth $20 or shoes worth $20, but they cannot buy both as they only have $20."
+            case 3: return "Therefore, they must make a choice, and the option they don’t choose becomes the Opportunity Cost."
+            case 4: return "If Person X chooses the books over the shoes, shoes will be the opportunity cost because it is the next best alternative."
+            case 5: return "Since you have only limited money due to scarcity, you can either produce chairs or tables. Which option is a better one for your furniture business?"
                 
             // Calculation #1 - Tabel will be displayed on main screen in a non-tabular form if possible
                 
-            default: "Error Loading Content"
+            default: return "Error Loading Content"
             }
             
         default: "Mega-Error"
         }
+        
+        return ""
+    }
+    
+    // Forward Button View
+    @IBAction func ForwardButtonAction(_ sender: UIButton) {
+        
+        if content(num: contentNum, level: contentLevel) != "Error Loading Content" {
+            
+            DialogueLabelView.text = content(num: contentNum, level: contentLevel)
+            contentNum += 1
+        } else {
+            
+            contentNum = 0
+            contentLevel += 1
+            DialogueLabelView.text = content(num: contentNum, level: contentLevel)
+        }
+    }
+    
+    // Backward Button View
+    @IBAction func BackButtonAction(_ sender: UIButton) {
+            
+            if contentNum > 0 {
+                
+                contentNum -= 1
+                DialogueLabelView.text = content(num: contentNum, level: contentLevel)
+            } else {
+                
+                if contentLevel > 0 {
+                    
+                    contentLevel -= 1
+                    contentNum = content[contentLevel]
+                    DialogueLabelView.text = content(num: contentNum, level: contentLevel)
+                }
+            }
+    
     }
     
     // Settings View
