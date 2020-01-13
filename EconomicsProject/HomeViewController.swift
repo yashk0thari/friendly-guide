6ed7317b90a4fb9cff271ffa84e0ffadca526038
Yash Kothari <yashkothari1000@gmail.com>
2020-01-13 21:32:05 +0530

    Level 6 done

--- EconomicsProject/HomeViewController.swift
+++ EconomicsProject/HomeViewController.swift
@@ -13,7 +13,7 @@ class HomeViewController: UIViewController {
     // Content Handler Variables
     var contentNum = 0
     var contentLevel = 0
-    let content : [Int] = [3, 5, 5]
+    let content : [Int] = [3, 5, 5, 3, 5, 10]
     
     // Instance Outlets
     @IBOutlet weak var ChangeNameTextFieldView: UITextField!
@@ -96,7 +96,56 @@ class HomeViewController: UIViewController {
             case 4: return "If Person X chooses the books over the shoes, shoes will be the opportunity cost because it is the next best alternative."
             case 5: return "Since you have only limited money due to scarcity, you can either produce chairs or tables. Which option is a better one for your furniture business?"
                 
-            // Calculation #1 - Tabel will be displayed on main screen in a non-tabular form if possible
+            // Calculation #1 - Table will be displayed on main screen in a non-tabular form if possible
+                
+            default: return "Error Loading Content"
+            }
+            
+        case 3:
+            switch num {
+                
+            // Level #4
+            case 0: return "In order to be environmentally friendly/sustainable, you decide to install solar panels to power your furniture factories/production facilities."
+            case 1: return "There are two main types of goods: Economic and Free goods. Economic goods have a price assigned to them. Thus, they have an Opportunity Cost. Every manufactured good is an economic good; for example, clothes, candy, shoes, pencils, and etc. Free goods are gifts of nature that you don’t have to pay for. For example, air, forests, etc."
+            case 2: return " Every manufactured good is an economic good; for example, clothes, candy, shoes, pencils, and etc. Free goods are gifts of nature that you don’t have to pay for. For example, air, forests, etc."
+            case 3: return "In the case of solar panels, which good can be considered as a free or economic good?"
+                
+            // Multiple Choice #3
+                
+            default: return "Error Loading Content"
+            }
+            
+        case 4:
+            switch num {
+                
+            // Level #5
+            case 0: return "Demand- the willingness and ability to buy a given good or service."
+            case 1: return "For example, Charlie wants to buy a laptop worth $500, meaning he is willing. However, he does not have $500, therefore he is NOT  able to buy the good."
+            case 2: return "If income rises, the demand for a normal good tends to rise. If the good is an inferior good, its demand will fall as income rises. Tastes and preferences are likings of people."
+            case 3: return "For example, if someone likes ice creams, it is likely that they will demand ice creams. A persuasive advertising campaign will encourage people to buy the given good. Related goods are of two types: complementary goods and substitute goods."
+            case 4: return "Complementary goods are those goods that are used together. For example, bread and butter. Substitute goods are goods that can be used in place of each other. For example, Coca-cola and Pepsi."
+            case 5: return "Your furniture company _______, is facing competition from another furniture company ‘AB Furnishings’ as they have just launched cheaper, and better quality sofas. What type of good is ‘AB Furnishings’ table and what effect will it have on your demand?"
+                
+            // Fill In the Blanks #1
+                
+            default: return "Error Loading Content"
+            }
+            
+        case 5:
+            switch num {
+                
+            // Level #3
+            case 0: return "Supply is the willingness and ability to sell a good or service."
+            case 1: return "Factors affecting supply are supply shocks, taxes, subsidies, technology, and cost of factors of production. Supply shocks are unexpected events that affect the supply of a good or s service."
+            case 2: return "For example, due to a war or a hurricane, chocolate cannot be exported by the Ivory Coast in Africa. Taxes in terms of supply are corporate taxes. Corporate taxes are aimed at a firm’s profits; a certain percentage of the firm’s profits are taken by the government."
+            case 3: return "For example, if the percentage of tax/ tax rate is increased, suppliers will supply less as a greater proportion of their profits will be taken by the government."
+            case 4: return "Subsidies are payments made by the government to producers to encourage the production of certain beneficial goods. For example, governments often provide subsidies to private firms providing education or healthcare since both services are beneficial to society."
+            case 5: return "Advancements in Technology increase the supply of a given good by improving efficiency. For example, before the printing press was introduced, all books were handwritten."
+            case 6: return "Thus, the production of books was a time-consuming and expensive process. With the printing press, however, the supply rose as efficiency increased. As studied earlier, factors of production are land, labour, capital, and enterprise."
+            case 7: return "The price of these factors determines the cost of producing the good. Therefore, if the price of the factors of production rises, the cost of production rises. Hence, making it expensive for the producer to produce and vice-versa."
+            case 8: return "Due to a hurricane in the city where your production facility is located, the supply of chairs has fallen because labour is not available. Is the cause of the fall in supply due to a fall in willingness or ability?"
+            case 9: return "Due to an increase in the corporate tax (a tax levied on firms), you are supplying fewer sofas because the incentive to sell sofas has fallen as you must pay a larger proportion of your profit to the government."
+            case 10: return "Has the supply fallen due to a fall in willingness or ability?"
                 
             default: return "Error Loading Content"
             }
